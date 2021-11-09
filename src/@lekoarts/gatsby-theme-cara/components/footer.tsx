2094cd252e58a8e5768de2a8a62961f99b9bd53d
Mattia Lobertini <lobetia@gmail.com>
2021-11-09 18:41:50 +0100

    Updated icon
    Signed-off-by: Mattia Lobertini <lobetia@gmail.com>

--- src/@lekoarts/gatsby-theme-cara/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -1,10 +1,14 @@
 /** @jsx jsx */
 import {Box, jsx} from "theme-ui"
+import logoRound from '../../../images/logo_round.svg'
 
 const Footer = () => {
   return (
     <Box as="footer" variant="footer" style={{fontSize: '13px'}}>
-      Copyright &copy; {new Date().getFullYear()}. Tutti i diritti riservati
+      <img src={logoRound} alt="FirenzeJS logo" width={64}/>
+      <br/>
+      <br/>
+      Copyright &copy; {new Date().getFullYear()}. Tutti i diritti riservati ai rispettivi autori
     </Box>
   )
 }
